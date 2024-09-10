37fa4e54d0fb238b5978f05c0aaca1e8a65c3f14
sharly-dev <carlos.carrero@javeriana.edu.co>
2024-09-10 12:10:02 -0500

    Carpetas y algunos recursos

--- app/src/main/java/com/cuatrodivinas/seekandsolve/Logica/MainActivity.kt
+++ app/src/main/java/com/cuatrodivinas/seekandsolve/Logica/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.cuatrodivinas.seekandsolve
+package com.cuatrodivinas.seekandsolve.Logica
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.cuatrodivinas.seekandsolve.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
