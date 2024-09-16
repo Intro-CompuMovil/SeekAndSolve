604504b1b1a343ac0b219717dc1eb0008c0a1f27
Édgar Julián González Sierra <edgarjuliangonzalezsierra167@gmail.com>
2024-09-16 17:07:28 -0500

    Cambios permisos camara

--- app/src/main/java/com/cuatrodivinas/seekandsolve/Datos/Data.kt
+++ app/src/main/java/com/cuatrodivinas/seekandsolve/Datos/Data.kt
@@ -1,4 +1,7 @@
 package com.cuatrodivinas.seekandsolve.Datos
 
 class Data {
+    companion object{
+        const val PERMISO_CAMARA = 0
+    }
 }
--- app/src/main/java/com/cuatrodivinas/seekandsolve/Logica/EditarPerfil.kt
+++ app/src/main/java/com/cuatrodivinas/seekandsolve/Logica/EditarPerfil.kt
@@ -1,14 +1,19 @@
 package com.cuatrodivinas.seekandsolve.Logica
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.text.InputType
 import android.view.MotionEvent
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.cuatrodivinas.seekandsolve.Datos.Data.Companion.PERMISO_CAMARA
 import com.cuatrodivinas.seekandsolve.R
 import com.cuatrodivinas.seekandsolve.databinding.ActivityEditarPerfilBinding
 
@@ -21,6 +26,7 @@ class EditarPerfil : AppCompatActivity() {
         binding = ActivityEditarPerfilBinding.inflate(layoutInflater)
         setContentView(binding.root)
         quemarDatos()
+        eventoCambiarFoto()
         eventoVolver()
         eventoAplicarCambios()
     }
@@ -34,15 +40,55 @@ class EditarPerfil : AppCompatActivity() {
         binding.FechaETxt.setHint("11/01/1865")
     }
 
+    private fun eventoCambiarFoto(){
+        binding.cambiarFotoBtn.setOnClickListener {
+            pedirPermiso(this, android.Manifest.permission.CAMERA,"Necesitamos el permiso de cámara para cambiar tu foto de perfil", PERMISO_CAMARA)
+        }
+    }
+
+    private fun pedirPermiso(context: Context, permiso: String, justificacion: String,
+                             idCode: Int){
+        if(ContextCompat.checkSelfPermission(context, permiso) !=
+            PackageManager.PERMISSION_GRANTED){
+            if (shouldShowRequestPermissionRationale(permiso)) {
+                // Explicar al usuario por qué necesitamos el permiso
+                Toast.makeText(context, justificacion, Toast.LENGTH_SHORT).show()
+            }
+            requestPermissions(arrayOf(permiso), idCode)
+        }
+        else{
+            Toast.makeText(context, "Permiso otorgado", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISO_CAMARA -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    Toast.makeText(this, "Permiso otorgado", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "FUNCIONALIDADES REDUCIDAS", Toast.LENGTH_LONG).show()
+                }
+                return
+            }
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
+
+
     private fun eventoVolver() {
         binding.volverBtn.setOnClickListener {
-            startActivity(Intent(this, VerPerfil::class.java))
+            finish()
         }
     }
 
     private fun eventoAplicarCambios() {
         binding.aplicarCambiosBtn.setOnClickListener {
-            startActivity(Intent(this, VerPerfil::class.java))
+            finish()
         }
     }
 }
