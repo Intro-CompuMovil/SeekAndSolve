3a6ba5fe3444af24cd2c3ebaeb1abee3377865a2
sharly-dev <carlos.carrero@javeriana.edu.co>
2024-11-16 15:58:37 -0500

    feat: VerPerfilActivity refactored and done
    - Comentado, reorganizado y trayendo la info del RealtimeDB
    - Eliminar todo lo del bundle

--- app/src/main/java/com/cuatrodivinas/seekandsolve/Logica/VerPerfil.kt
+++ app/src/main/java/com/cuatrodivinas/seekandsolve/Logica/VerPerfil.kt
@@ -1,164 +1,133 @@
 package com.cuatrodivinas.seekandsolve.Logica
 
-import android.content.Context
 import android.content.Intent
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.cuatrodivinas.seekandsolve.R
 import com.cuatrodivinas.seekandsolve.databinding.ActivityVerPerfilBinding
-import android.graphics.drawable.Drawable
-import android.util.Base64
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import org.json.JSONArray
-import org.json.JSONObject
+import com.cuatrodivinas.seekandsolve.Datos.Data.Companion.PATH_USERS
+import com.cuatrodivinas.seekandsolve.Datos.Data.Companion.auth
+import com.cuatrodivinas.seekandsolve.Datos.Data.Companion.database
+import com.cuatrodivinas.seekandsolve.Datos.Usuario
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import java.io.File
-import java.io.FileInputStream
-import kotlin.properties.Delegates
 
 class VerPerfil : AppCompatActivity() {
     private lateinit var binding: ActivityVerPerfilBinding
-
-    private var id by Delegates.notNull<Int>()
-    private lateinit var nombre: String
-    private lateinit var username: String
-    private lateinit var correo: String
-    private lateinit var contrasena: String
-    private lateinit var fotoUrl: String
-    private lateinit var fechaNacimiento: String
-    private lateinit var externo: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityVerPerfilBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val bundle = intent.extras
-        if (bundle != null) {
-            id = bundle.getInt("id")
-            nombre = bundle.getString("nombre") ?: ""
-            username = bundle.getString("username") ?: ""
-            correo = bundle.getString("correo") ?: ""
-            contrasena = bundle.getString("contrasena") ?: ""
-            fotoUrl = bundle.getString("fotoUrl") ?: ""
-            fechaNacimiento = bundle.getString("fechaNacimiento") ?: ""
-            externo = bundle.getString("externo") ?: ""
+
+        // Si hay un usuario autenticado, cargar sus datos
+        val uid = auth.currentUser?.uid
+        if (uid != null) {
+            cargarDatosUsuario(uid)
+        } else {
+            showToast("No se encontró un usuario autenticado.")
+            navigateToMain()
         }
-        if(externo == "true"){
-            binding.editarPerfilBtn.visibility = android.view.View.GONE
-        }
-        inicializarComponentes()
-        quemarDatos()
-        eventoRecompensas()
-        eventoEstadisticas()
-        eventoVolver()
-        eventoEditarPerfil()
-        eventoLogout()
+        deshabilitarEditTexts()
+        configurarEventos()
     }
 
-    private fun inicializarComponentes(){
+    // Con el UID del usuario, obtener su información
+    private fun cargarDatosUsuario(uid: String) {
+        val userRef = database.getReference(PATH_USERS).child(uid)
+
+        userRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // Convierte el snapshot a la clase User
+                val usuario = snapshot.getValue(Usuario::class.java)
+                val nombre = usuario!!.nombre
+                val username = usuario.nombreUsuario
+                val correo = usuario.correo
+                val imagenUrl = usuario.imagenUrl
+                val fechaNacimiento = usuario.fechaNacimiento
+
+                actualizarUI(nombre, username, correo, imagenUrl, fechaNacimiento)
+                Log.d("Usuario", "Datos actualizados del usuario: $username")
+            }
+            override fun onCancelled(error: DatabaseError) {
+                Log.e("DatabaseError", "Error al suscribirse a los datos del usuario: ${error.message}")
+            }
+        })
+    }
+
+    private fun deshabilitarEditTexts(){
         binding.nombreETxt.isEnabled = false
         binding.corrreoETxt.isEnabled = false
         binding.FechaETxt.isEnabled = false
     }
 
-    private fun quemarDatos(){
+    private fun actualizarUI(nombre: String, username: String, correo: String, fotoUrl: String, fechaNacimiento: String) {
+        // Actualizar los campos de texto con la info del usuario
         binding.nombreUsuarioTxt.text = username
-        if (fotoUrl.isNotEmpty()) {
-            if (fotoUrl.startsWith("/")) {
-                // Cargar imagen desde el archivo
-                val file = File(fotoUrl)
-                if (file.exists()) {
-                    Glide.with(this)
-                        .load(file)
-                        .circleCrop() // Hacer la imagen circular
-                        .into(binding.imagenPerfil) // Establecer la imagen en el ImageView
-                } else {
-                    // Caso: archivo no existe, cargar imagen por defecto
-                    binding.imagenPerfil.imageTintList = ContextCompat.getColorStateList(this, R.color.primaryColor)
-                }
-            } else {
-                // Caso: cargar imagen por defecto si la fotoUrl no es un archivo válido
-                binding.imagenPerfil.imageTintList = ContextCompat.getColorStateList(this, R.color.primaryColor)
-            }
-        } else {
-            // Caso: fotoUrl está vacía, cargar imagen por defecto
-            binding.imagenPerfil.imageTintList = ContextCompat.getColorStateList(this, R.color.primaryColor)
-        }
         binding.nombreETxt.setText(nombre)
         binding.corrreoETxt.setText(correo)
         binding.FechaETxt.setText(fechaNacimiento)
+
+        // Manejar la foto de perfil
+        if (fotoUrl.isNotEmpty() && fotoUrl.startsWith("/")) {
+            val file = File(fotoUrl)
+            if (file.exists()) {
+                Glide.with(this)
+                    .load(file)
+                    .circleCrop()
+                    .into(binding.imagenPerfil)
+                return
+            }
+        }
+        // Si fotoUrl está vacío, el archivo no existe o no es válido
+        // Cargar imagen por defecto
+        binding.imagenPerfil.imageTintList = ContextCompat.getColorStateList(this, R.color.primaryColor)
     }
 
-    private fun eventoRecompensas() {
+    private fun configurarEventos() {
         binding.recompensasBtn.setOnClickListener {
             startActivity(Intent(this, RecompensasCarrera::class.java))
         }
-    }
 
-    private fun eventoEstadisticas() {
         binding.estadisticasBtn.setOnClickListener {
             startActivity(Intent(this, EstadisticasCarreras::class.java))
         }
-    }
 
-    private fun eventoVolver() {
         binding.backButtonUser.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            val bundle = Bundle()
-            bundle.putInt("id", id)
-            bundle.putString("nombre", nombre)
-            bundle.putString("username", username)
-            bundle.putString("correo", correo)
-            bundle.putString("contrasena", contrasena)
-            bundle.putString("fotoUrl", fotoUrl)
-            bundle.putString("fechaNacimiento", fechaNacimiento)
-            intent.putExtras(bundle)
-            startActivity(intent)
+            startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
-    }
 
-    private fun eventoLogout() {
         binding.logOutBtn.setOnClickListener {
-            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build()
-            val googleSignInClient = GoogleSignIn.getClient(this, gso)
-            val sharedPreferences = getSharedPreferences("user_session", Context.MODE_PRIVATE)
-            with(sharedPreferences.edit()) {
-                remove("session_id")
-                apply()
-            }
-            googleSignInClient.signOut().addOnCompleteListener(this) {
-                googleSignInClient.revokeAccess().addOnCompleteListener(this) {
-                    val intent = Intent(this, LoginActivity::class.java)
-                    startActivity(intent)
-                    finish()
-                }
-            }
+            cerrarSesion()
+        }
+
+        binding.editarPerfilBtn.setOnClickListener {
+            startActivity(Intent(this, EditarPerfil::class.java))
         }
     }
 
-    private fun eventoEditarPerfil() {
-        binding.editarPerfilBtn.setOnClickListener {
-            val intent = Intent(this, EditarPerfil::class.java)
-            val bundle = Bundle()
-            bundle.putInt("id", id)
-            bundle.putString("nombre", nombre)
-            bundle.putString("username", username)
-            bundle.putString("correo", correo)
-            bundle.putString("contrasena", contrasena)
-            bundle.putString("fotoUrl", fotoUrl)
-            bundle.putString("fechaNacimiento", fechaNacimiento)
-            intent.putExtras(bundle)
-            startActivity(intent)
-        }
+    private fun cerrarSesion() {
+        auth.signOut()
+        val intent = Intent(this, LandingActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        startActivity(intent)
+        finish()
+    }
+
+    private fun navigateToMain() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
