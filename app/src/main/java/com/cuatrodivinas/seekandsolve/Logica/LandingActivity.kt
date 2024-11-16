82bd0e32fd8e8bb8e4e4c9af0be13968261bf40c
sharly-dev <carlos.carrero@javeriana.edu.co>
2024-11-15 22:05:57 -0500

    fix: use binding on Landing

--- app/src/main/java/com/cuatrodivinas/seekandsolve/Logica/LandingActivity.kt
+++ app/src/main/java/com/cuatrodivinas/seekandsolve/Logica/LandingActivity.kt
@@ -8,34 +8,33 @@ import com.cuatrodivinas.seekandsolve.R
 import android.content.Intent
 import android.widget.Button
 import com.cuatrodivinas.seekandsolve.Datos.Data.Companion.auth
+import com.cuatrodivinas.seekandsolve.databinding.ActivityLandingBinding
+import com.cuatrodivinas.seekandsolve.databinding.ActivityMainBinding
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
 import java.io.File
 
 class LandingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLandingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_landing)
+        binding = ActivityLandingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setupAnimation()
     }
 
     override fun onResume() {
         super.onResume()
-        setupButton()
-    }
-
-    private fun setupAnimation() {
-        val cofreLaunch: ImageView = findViewById(R.id.cofreLaunch)
-        val jumpAnimation = AnimationUtils.loadAnimation(this, R.anim.jump)
-        cofreLaunch.startAnimation(jumpAnimation)
-    }
-
-    private fun setupButton() {
-        val button: Button = findViewById(R.id.comienzaTuAventura)
-        button.setOnClickListener {
+        binding.comienzaTuAventura.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
             finish()
         }
+    }
+
+    private fun setupAnimation() {
+        val jumpAnimation = AnimationUtils.loadAnimation(this, R.anim.jump)
+        binding.cofreLaunch.startAnimation(jumpAnimation)
     }
 }
