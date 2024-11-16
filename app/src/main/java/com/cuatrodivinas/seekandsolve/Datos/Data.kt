0c4a9a834f6642833a7913fe2b424e25e288055e
sharly-dev <carlos.carrero@javeriana.edu.co>
2024-11-15 22:13:03 -0500

    update: Data with paths RealtimeDB
    - Y definir database en el companion pq pa todas las pantallas es el mismo database

--- app/src/main/java/com/cuatrodivinas/seekandsolve/Datos/Data.kt
+++ app/src/main/java/com/cuatrodivinas/seekandsolve/Datos/Data.kt
@@ -3,17 +3,25 @@ package com.cuatrodivinas.seekandsolve.Datos
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class Data {
     companion object{
         const val PERMISO_CAMARA = 0
         const val PERMISO_GALERIA = 1
         const val MY_PERMISSION_REQUEST_CAMERA = 2
-        const val PATH_USERS = "usuarios/"
+        const val PATH_USERS = "usuarios"
+        const val PATH_DESAFIOS = "desafios"
+        const val PATH_CARRERAS = "carreras"
+        const val PATH_RECOMPENSAS = "recompensas"
+        const val PATH_PREGUNTAS = "preguntas"
         lateinit var auth: FirebaseAuth
+        lateinit var database: FirebaseDatabase
     }
 
     init {
         auth = Firebase.auth
+        database = FirebaseDatabase.getInstance()
     }
 }
