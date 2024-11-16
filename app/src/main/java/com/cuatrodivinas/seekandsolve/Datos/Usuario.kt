30f4a08af3e93bd4f883c5bc58c81da2262b0b06
sharly-dev <carlos.carrero@javeriana.edu.co>
2024-11-15 22:05:14 -0500

    fix: Usuario model (add amigos ArrayList)
    - En la base de datos se guardará la lista de amigos dentro de cada usuario

--- app/src/main/java/com/cuatrodivinas/seekandsolve/Datos/Usuario.kt
+++ app/src/main/java/com/cuatrodivinas/seekandsolve/Datos/Usuario.kt
@@ -1,8 +1,7 @@
 package com.cuatrodivinas.seekandsolve.Datos;
 
 class Usuario(var nombre: String, var nombreUsuario: String, var correo: String, var password: String,
-                    var imagenUrl: String,
-                     var fechaNacimiento: String) {
+                    var imagenUrl: String, var fechaNacimiento: String, var amigos: ArrayList<String> = ArrayList()) {
 
     constructor() : this("", "", "", "", "", "")
 }
