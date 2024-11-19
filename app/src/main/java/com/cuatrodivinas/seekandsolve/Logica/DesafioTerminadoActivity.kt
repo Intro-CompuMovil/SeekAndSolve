b0fe1ff4403b7972e7b3e3af0cadf8322d49a058
Édgar Julián González Sierra <edgarjuliangonzalezsierra167@gmail.com>
2024-11-18 23:36:06 -0500

    Funcion innecesaria removida

--- app/src/main/java/com/cuatrodivinas/seekandsolve/Logica/DesafioTerminadoActivity.kt
+++ app/src/main/java/com/cuatrodivinas/seekandsolve/Logica/DesafioTerminadoActivity.kt
@@ -67,20 +67,6 @@ class DesafioTerminadoActivity : AppCompatActivity() {
         inicializarElementos()
     }
 
-    fun calcularDistancia(lat1: Double, lon1: Double, lat2: Double, lon2: Double): Double {
-        val radioTierra = 6371000.0  // Radio de la Tierra en metros
-        val dLat = Math.toRadians(lat2 - lat1)
-        val dLon = Math.toRadians(lon2 - lon1)
-
-        val a = sin(dLat / 2) * sin(dLat / 2) +
-                cos(Math.toRadians(lat1)) * cos(Math.toRadians(lat2)) *
-                sin(dLon / 2) * sin(dLon / 2)
-
-        val c = 2 * atan2(sqrt(a), sqrt(1 - a))
-
-        return radioTierra * c  // Distancia en metros
-    }
-
     private fun inicializarElementos(){
         //Obtener la pregunta random
         val recompensa: Recompensa = Recompensa("1","Capitan Cuac Cuac")
