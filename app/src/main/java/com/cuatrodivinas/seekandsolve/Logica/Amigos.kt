304b1eec5df5d9be68cf54479758cb5abfe2bbef
DFallenKnight <168678995+DFallenKnight@users.noreply.github.com>
2024-11-18 23:09:32 -0500

    Carga de amigos de firebase

--- app/src/main/java/com/cuatrodivinas/seekandsolve/Logica/Amigos.kt
+++ app/src/main/java/com/cuatrodivinas/seekandsolve/Logica/Amigos.kt
@@ -4,17 +4,23 @@ import android.content.Intent
 import android.database.Cursor
 import android.database.MatrixCursor
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.cuatrodivinas.seekandsolve.Datos.Data.Companion.PATH_USERS
 import com.cuatrodivinas.seekandsolve.Datos.Data.Companion.auth
 import com.cuatrodivinas.seekandsolve.Datos.Data.Companion.database
+import com.cuatrodivinas.seekandsolve.Datos.Usuario
 import com.cuatrodivinas.seekandsolve.databinding.ActivityAmigosBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import org.json.JSONArray
 import java.io.FileInputStream
 
 class Amigos : AppCompatActivity() {
     private lateinit var binding: ActivityAmigosBinding
     private lateinit var amigosJsonArray: JSONArray
+    private var amigos: MutableMap<String, String> = mutableMapOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,25 +45,18 @@ class Amigos : AppCompatActivity() {
     }
 
     private fun setListAmigos() {
-        val columns = arrayOf("_id", "idUser", "nombre")
-        val matrixCursor = MatrixCursor(columns)
-        var idCounter = 1L
-
         //obtener amigos del usuario actual de firebase
-        val refAmigos = database.getReference("$PATH_USERS/${auth.currentUser?.uid}/amigos")
-        refAmigos.get().addOnSuccessListener { dataSnapshot ->
-            val amigos = dataSnapshot.value as? Map<String, String>
-            if (amigos != null) {
-                for ((amigoId, amigoNombre) in amigos) {
-                    matrixCursor.addRow(arrayOf(idCounter, amigoId, usernameByID(amigoId)))
-                    idCounter++
-                }
-                val cursor: Cursor = matrixCursor
-                val amigosAdapter = AmigosAdapter(this, cursor, 0)
-                binding.amigosLv.adapter = amigosAdapter
+        val refAmigos = database.getReference("usuarios/${auth.currentUser?.uid}/amigos/")
+        refAmigos.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                Toast.makeText(this@Amigos, "Cargando amigos...", Toast.LENGTH_SHORT).show()
+                val amigosUids = snapshot.children.mapNotNull { it.value as? String }
+                fetchAmigosInfo(amigosUids)
             }
-        }
-
+            override fun onCancelled(error: DatabaseError) {
+                // Handle error
+            }
+        })
 
         /*for (i in 0 until amigosJsonArray.length()) {
             val jsonObject = amigosJsonArray.getJSONObject(i)
@@ -69,6 +68,34 @@ class Amigos : AppCompatActivity() {
         val cursor: Cursor = matrixCursor
         val amigosAdapter = AmigosAdapter(this, cursor, 0)
         binding.amigosLv.adapter = amigosAdapter*/
+    }
+
+    private fun fetchAmigosInfo(amigosUids: List<String>) {
+        amigosUids.forEach { uid ->
+            val amigoRef = database.getReference("$PATH_USERS/$uid")
+            amigoRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val amigo = snapshot.getValue(Usuario::class.java)
+                    amigo?.let {
+                        amigos.put(uid, amigo.nombreUsuario)
+                        val columns = arrayOf("_id", "idUser", "nombre")
+                        val matrixCursor = MatrixCursor(columns)
+                        var idCounter = 1L
+                        amigos.forEach { (id, name) ->
+                            matrixCursor.addRow(arrayOf(idCounter, id, name))
+                            idCounter++
+                        }
+                        val cursor: Cursor = matrixCursor
+                        val amigosAdapter = AmigosAdapter(this@Amigos, cursor, 0)
+                        binding.amigosLv.adapter = amigosAdapter
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    // Handle error
+                }
+            })
+        }
     }
 
     private fun usernameByID(id: String): String {
